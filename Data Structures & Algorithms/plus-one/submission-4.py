77016783ca5f36eaba2d2b3a8e27dbb401d42028
Parimal <56225634+parimalmahindrakar@users.noreply.github.com>
2026-05-02 12:44:45 +0530

    Add: plus-one - submission-4

---
+++ Data Structures & Algorithms/plus-one/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        new_arr = []
+        # for i in range(len(digits) - 1, -1, -1):
+
+        for i in digits[::-1]:
+            summation = i + carry
+            reminder = summation % 10
+            ques = summation // 10
+            carry = ques
+            new_arr.append(reminder)
+
+        if carry:
+            new_arr.append(carry)
+
+        return new_arr[::-1]
+
+            
+
+
+
+
+
+        
